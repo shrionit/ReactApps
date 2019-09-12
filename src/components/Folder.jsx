a28a9fc5b6faa4ad9543f9b5478d3a8389eceaf5
Shriprakash <sptiwari46289@gmail.com>
2019-09-12 23:06:40 +0530

    starting to create folder component

--- src/components/Folder.jsx
+++ src/components/Folder.jsx
@@ -1,5 +1,13 @@
 import React, {Component} from 'React';
 
 class Folder extends Component{
-  
+  constructor(props){
+    super(props);
+    var fs = require('fs');
+    var files = fs.readdirSync('/assets/');
+  }
+
+  render(){
+
+  }
 }
