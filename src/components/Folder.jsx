f82c9eaf01ad4d7e6227aa9048e82cc47edf1e31
Shriprakash <sptiwari46289@gmail.com>
2019-09-12 23:32:32 +0530

    added new components

--- src/components/Folder.jsx
+++ src/components/Folder.jsx
@@ -1,16 +1,35 @@
-import React, {Component} from 'React';
+import React, {Component} from 'react';
+import Open from './Open.jsx';
+import './Folder.css';
 var fs = require('fs');
+
+class VFile extends Component{
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return (
+      'hello'
+    );
+  }
+}
+
 class Folder extends Component{
   constructor(props){
     super(props);
     this.stats = {
-      folders: fs.readdirSync('/assets/')
+      folders: 'hello'
     };
   }
 
   render(){
-    <div>
-      ${this.stats.folders}
-    </div>
+    return (
+      <div>
+        {this.stats.folders}
+        <Open />
+      </div>
+    );
   }
 }
+
+export default Folder;
---
+++ src/components/Open.jsx
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import './Open.css';
+
+class Open extends Component{
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return (
+      <div id='open'>
+        This is good
+      </div>
+    );
+  }
+}
+
+export default Open;
